0fb61e1dc77b9d46b21de6808951a375f65b269e
PriyankaBedse <88850595+PriyankaBedse@users.noreply.github.com>
2024-03-27 14:37:19 +0530

    Update raw_customer.sql

--- models/raw/raw_customer.sql
+++ models/raw/raw_customer.sql
@@ -1,1 +1,8 @@
-
+{{
+    config(
+        materialized='table'
+    )
+}}
+select CUSTOMER_ID, CUSTOMER_NAME 
+from TERRAFORM_DESTINATION_DATABASE.TRANSFORMED_CUSTOMER.TERRAFORM_TABLE 
+where STATE= 'Texas'
